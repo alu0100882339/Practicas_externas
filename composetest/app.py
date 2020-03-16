9740bde790464600d04b153ea97f55e0f57c0012
EC2 Default User <ec2-user@ip-172-31-4-218.eu-west-1.compute.internal>
2020-03-16 18:56:24 +0000

    Docker y Docker compose

---
+++ composetest/app.py
@@ -0,0 +1,26 @@
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+# hostname del contenedor de redis en la red de la aplicación
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
